5d2a76b0f67215bd237c9c48598cac6c339124c3
Nanagyamera <nanagyamera3@gmail.com>
2023-07-13 15:50:06 -0700

    Added SQL script to create users table with country enumeration

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Create table users
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);

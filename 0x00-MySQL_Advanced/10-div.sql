3d4055a9429d672ec70fe55f331d4d8faa10d9aa
Nanagyamera <nanagyamera3@gmail.com>
2023-07-13 18:45:34 -0700

    Added function SafeDiv to handle safe division

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,11 +1,14 @@
 -- Create function SafeDiv
-CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS INT
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER $$
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
 BEGIN
-    IF b = 0 THEN
-        RETURN 0;
-    ELSE
-        RETURN a / b;
-    END IF;
-END;
+    DECLARE result FLOAT DEFAULT 0;
 
+    IF b != 0 THEN
+        SET result = a / b;
+    END IF;
+    RETURN result;
+END $$
+DELIMITER ;

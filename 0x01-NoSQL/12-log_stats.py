9046c2c3c8669008763c4df7239ede115fadf625
Nanagyamera <nanagyamera3@gmail.com>
2023-07-18 08:19:44 -0700

    provides some stats about Nginx logs stored in MongoDB

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Task12
+"""
+
+
+from pymongo import MongoClient
+
+# Establish a connection to the MongoDB server
+client = MongoClient('mongodb://localhost:27017')
+
+# Select the database and collection
+db = client['logs']
+collection = db['nginx']
+
+# Get the total number of documents in the collection
+total_logs = collection.count_documents({})
+print(f"first line: {total_logs} logs where {total_logs} is the number of documents in this collection")
+
+# Get the number of documents with each HTTP method
+http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+print("second line: Methods:")
+for method in http_methods:
+    count = collection.count_documents({"method": method})
+    print(f"\t{count} logs with method = {method}")
+
+# Get the number of documents with specific method and path
+specific_logs = collection.count_documents({"method": "GET", "path": "/status"})
+print(f"method=GET, path=/status: {specific_logs} logs")
+
+# Close the MongoDB connection
+client.close()

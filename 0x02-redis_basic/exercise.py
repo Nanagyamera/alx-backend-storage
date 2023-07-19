19584dc2d795eb786b04dd9271fa58c0937d36d9
Nanagyamera <nanagyamera3@gmail.com>
2023-07-19 07:31:45 -0700

    Add replay function to display history of calls and update Cache class with call_history decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,6 +8,21 @@ from typing import Union, Callable
 from functools import wraps
 
 
+def count_calls(method: Callable) -> Callable:
+    """
+    Tracks the number of calls made to a method in a Cache class.
+    """
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        """
+        Invokes the given method after incrementing its call counter.
+        """
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return invoker
+
+
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()

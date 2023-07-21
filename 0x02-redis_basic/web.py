09acd922d958625739eef89f811c5e41a12dc3a6
Nanagyamera <nanagyamera3@gmail.com>
2023-07-21 13:54:42 -0700

    Implement get_page function with URL access tracking and caching functionality

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,36 +8,25 @@ from functools import wraps
 from typing import Callable
 
 
-redis_store = redis.Redis()
-"""
-Redis instance.
-"""
-
-
-def data_cacher(method: Callable) -> Callable:
-    """
-    Caches the output of fetched data.
-    """
+def cache_with_expiration(method: Callable) -> Callable:
     @wraps(method)
-    def invoker(url) -> str:
-        """
-        The wrapper function for caching the output.
-        """
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+    def wrapper(url: str) -> str:
+        cache_key = f"count:{url}"
+        html_content = redis_client.get(url)
+        if html_content is None:
+            html_content = method(url)
+            # Cache the HTML content with an expiration of 10 seconds
+            redis_client.set(url, html_content, ex=10)
+        # Increment the cache count for the URL
+        redis_client.incr(cache_key)
+        return html_content
 
+    return wrapper
 
-@data_cacher
+@cache_with_expiration
 def get_page(url: str) -> str:
-    """
-    Returns the content of a URL after caching and tracking
-    the request's response
-    """
-    return requests.get(url).text
+    response = requests.get(url)
+    return response.text
+
+# Set up Redis client
+redis_client = redis.Redis()
